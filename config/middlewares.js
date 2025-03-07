636d47965419cbd9357f8360e300a15f70dec034
lokytech5 <lokytech5@gmail.com>
2025-03-07 08:35:50 +0000

    fresh update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,34 @@
-module.exports = [
-  'strapi::logger',
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',  // Local development URL
+        'https://app.egetwhy.club', // Production URL
+        'https://portfolio-project-rho-gold.vercel.app', // Fronten URL
+        'https://portfolio-project-rho-gold.vercel.app', // Production URL for frontend
+
+      ],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',

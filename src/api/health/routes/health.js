a3b71440c21fbcfbafb3fabfe4f0441b60cd14ff
lokytech5 <lokytech5@gmail.com>
2025-04-02 21:56:05 +0100

    update route endpoint for handling get method

--- src/api/health/routes/health.js
+++ src/api/health/routes/health.js
@@ -1,13 +1,12 @@
 module.exports = {
   routes: [
-    // {
-    //  method: 'GET',
-    //  path: '/health',
-    //  handler: 'health.exampleAction',
-    //  config: {
-    //    policies: [],
-    //    middlewares: [],
-    //  },
-    // },
-  ],
+    {
+      method: 'GET',
+      path: '/health',
+      handler: 'health.index',
+      config: {
+        auth: false
+      }
+    }
+  ]
 };

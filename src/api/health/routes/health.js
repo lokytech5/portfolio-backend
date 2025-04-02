fe9a86cfc451d8021324e4a26f70ee92ce00a962
lokytech5 <lokytech5@gmail.com>
2025-04-02 21:53:44 +0100

    created health api endpoint via npx generate

---
+++ src/api/health/routes/health.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/health',
+    //  handler: 'health.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};

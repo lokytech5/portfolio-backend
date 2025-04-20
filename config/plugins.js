dcb49f0e085526bb8409325b1d5c6c748c40a834
lokytech5 <lokytech5@gmail.com>
2025-04-21 00:12:29 +0100

    added s3 plugin and update plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,18 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: "@strapi/provider-upload-aws-s3",
+        providerOptions: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+            ACL: null
+          },
+          baseUrl: `https://${env("AWS_BUCKET_NAME")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+        },
+      },
+    },
+  });
+  
